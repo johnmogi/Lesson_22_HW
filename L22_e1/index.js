453d35a6edf5acf760dfe1897cd1b24016d245c0
johnmogi <anguru@gmail.com>
2019-10-24 22:58:35 +0300

    1st cut is the deepest

--- L22_e1/index.js
+++ L22_e1/index.js
@@ -1,5 +1,18 @@
-$(() => {
+$(document).ready(function() {
+  //   var selecta = $("#selecta");
 
+  $("#selecta").click(function() {
+    setTimeout(() => {
+      $(".blue").toggleClass(function() {
+        if ($(this)) {
+          return "opacityempty";
+        } else {
+          return "opacityfull";
+        }
+      });
 
-    alert("yo")
+      //   $(".blue").toggle(200, "");
+      //   $(".blue").css("opacity", "0");
+    }, 500);
+  });
 }); // *RF
